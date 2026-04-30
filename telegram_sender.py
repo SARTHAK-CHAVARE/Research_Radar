6bbef46fbfc127b20577203a484eec4be4d9ca5b
smriti.patel <smriti.patel@oracle.com>
2026-05-01 04:24:32 +0530

    Add Telegram sender

---
+++ telegram_sender.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot8501689777:AAGQfPYHFbcWuBwgagN-_7qZWdGAHW_iJUI/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    return response.json()
+
+
+if __name__ == "__main__":
+    send_message("ResearchRadar is working 🚀")
